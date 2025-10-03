b014c2db1539d3fe29387f7bd3f68a339e602c69
mkstas <makarov.stn@gmail.com>
2025-10-03 13:15:45 +0300

    Add project files.

---
+++ FisherApp.WPF/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FisherApp.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenAboutWindow_Click(object sender, RoutedEventArgs e)
+        {
+            AboutWindow aboutWindow = new();
+            aboutWindow.ShowDialog();
+        }
+
+        private void OpenAuthorWindow_Click(object sender, RoutedEventArgs e)
+        {
+            AuthorWindow authorWindow = new();
+            authorWindow.ShowDialog();
+        }
+    }
+}
